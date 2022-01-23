ee39d37cbcd9d2e84dca1e8fda187194442ffd99
Nick Dobos <ninjoala@gmail.com>
2022-01-22 19:02:11 -0500

    changing solution

---
+++ baseline_blazor/Client/Pages/CustomComponentBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+using baseline_blazor.Client.Extensions;
+
+
+namespace baseline_blazor.Client.Pages
+{
+    public class CustomComponentBase : ComponentBase, IDisposable
+    {
+        [Inject] NavigationManager NavManager { get; set; }
+        [Inject] IJSRuntime JsRuntime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavManager.LocationChanged += TryFragmentNavigation;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await NavManager.NavigateToFragmentAsync(JsRuntime);
+        }
+
+        private async void TryFragmentNavigation(object sender, LocationChangedEventArgs args)
+        {
+            await NavManager.NavigateToFragmentAsync(JsRuntime);
+        }
+
+        public void Dispose()
+        {
+            NavManager.LocationChanged -= TryFragmentNavigation;
+        }
+    }
+}
